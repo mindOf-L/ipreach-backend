3e527b099b2e81aeceb3d3992ec204681dc2765f
L <eleaz.rs@gmail.com>
2025-03-18 15:28:30 +0100

    map enumerated of EShiftUserRole in shiftAssignment correctly for h2

--- src/main/java/app/ipreach/backend/shifts/db/model/ShiftAssignment.java
+++ src/main/java/app/ipreach/backend/shifts/db/model/ShiftAssignment.java
@@ -4,6 +4,8 @@ import app.ipreach.backend.shared.enums.EShiftUserRole;
 import app.ipreach.backend.users.db.model.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -37,6 +39,7 @@ public class ShiftAssignment{
     @ManyToOne(fetch = FetchType.EAGER)
     private User user;
 
+    @Enumerated(EnumType.STRING)
     private EShiftUserRole shiftUserRole;
 
 }
