56293d1634f0ae2a3e5ef33a0e23a71d7aa56a32
L <eleaz.rs@gmail.com>
2025-03-25 15:19:17 +0100

    create example data for shift with assignment

--- src/main/java/app/ipreach/backend/core/config/InitializingCreateDBData.java
+++ src/main/java/app/ipreach/backend/core/config/InitializingCreateDBData.java
@@ -3,10 +3,10 @@ package app.ipreach.backend.core.config;
 import app.ipreach.backend.shared.enums.ERole;
 import app.ipreach.backend.shared.enums.EShiftUserRole;
 import app.ipreach.backend.shifts.db.model.ShiftAssignment;
+import app.ipreach.backend.shifts.db.repository.ShiftAssigmentRepository;
 import app.ipreach.backend.shifts.db.repository.ShiftRepository;
 import app.ipreach.backend.users.db.model.User;
 import app.ipreach.backend.users.db.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
@@ -31,6 +31,7 @@ public class InitializingCreateDBData { //implements ApplicationListener<Migrati
 
     private final UserRepository userRepository;
     private final ShiftRepository shiftRepository;
+    private final ShiftAssigmentRepository shiftAssignmentRepository;
 
     @Value("${DB_NAME:mydb}")
     private String dbName;
@@ -57,22 +58,21 @@ public class InitializingCreateDBData { //implements ApplicationListener<Migrati
     public InitializingBean createDBDataOnAppReady() {
         // Check if we're in a profile where migration doesn't happen
         log.info("Starting database initialization (unique stage)...");
-        return () -> createExampleData();
 
-    }
-
-    @Transactional
-    public void createExampleData() {
-        if (!loadInitialData) return;
-
-        log.info("Initializing database...💿");
         if(driverClassName.equals("org.postgresql.Driver")) {
             // execute when changing database version
             jdbcTemplate.execute("ALTER DATABASE \"%s\" REFRESH COLLATION VERSION;".formatted(dbName));
             // set accent-insensitive on searches
             jdbcTemplate.execute("CREATE EXTENSION IF NOT EXISTS unaccent;");
-            jdbcTemplate.execute("CREATE EXTENSION IF NOT EXISTS unaccent;");
         }
+
+        return () -> createExampleData();
+    }
+
+    private void createExampleData() {
+        if (!loadInitialData) return;
+
+        log.info("Initializing database...💿");
 
         userRepository.saveAndFlush(User.builder()
             .name("admin")
@@ -88,44 +88,31 @@ public class InitializingCreateDBData { //implements ApplicationListener<Migrati
         var usersFullShift = userRepository.giveMeRandomParticipants(4);
 
         for (int i = 0; i < usersFullShift.size(); i++) {
-            switch (i) {
-                case 0 -> fullShift.getAssignments().add(ShiftAssignment.builder()
-                    .shiftId(fullShift.getId())
-                    .user(usersFullShift.get(i))
-                    .shiftUserRole(EShiftUserRole.OVERSEER)
-                    .build());
-                case 1 -> fullShift.getAssignments().add(ShiftAssignment.builder()
-                    .shiftId(fullShift.getId())
-                    .user(usersFullShift.get(i))
-                    .shiftUserRole(EShiftUserRole.AUXILIAR)
-                    .build());
-                default -> fullShift.getAssignments().add(ShiftAssignment.builder()
-                    .shiftId(fullShift.getId())
-                    .user(usersFullShift.get(i))
-                    .shiftUserRole(EShiftUserRole.PARTICIPANT)
-                    .build());
-            }
+            fullShift.getAssignments().add(shiftAssignmentRepository.save(ShiftAssignment.builder()
+                .shiftId(fullShift.getId())
+                .user(usersFullShift.get(i))
+                .shiftUserRole(i == 0 ? EShiftUserRole.OVERSEER :
+                               i == 1 ? EShiftUserRole.AUXILIAR :
+                                        EShiftUserRole.PARTICIPANT)
+                .build()));
         }
+
         shiftRepository.saveAndFlush(fullShift);
         // partial filled with participants
-        shiftRepository.findShiftById(2L).orElseThrow(() -> new RuntimeException("Shift not found"));
+        var partialShift = shiftRepository.findShiftById(2L).orElseThrow(() -> new RuntimeException("Shift not found"));
 
-        usersFullShift = userRepository.giveMeRandomParticipants(2);
+        var usersPartialShift = userRepository.giveMeRandomParticipants(2);
 
-        for (int i = 0; i < usersFullShift.size(); i++) {
-            switch (i) {
-                case 0 -> fullShift.getAssignments().add(ShiftAssignment.builder()
-                    .shiftId(fullShift.getId())
-                    .user(usersFullShift.get(i))
-                    .shiftUserRole(EShiftUserRole.OVERSEER)
-                    .build());
-                default -> fullShift.getAssignments().add(ShiftAssignment.builder()
-                    .shiftId(fullShift.getId())
-                    .user(usersFullShift.get(i))
-                    .shiftUserRole(EShiftUserRole.PARTICIPANT)
-                    .build());
-            }
+        for (int i = 0; i < usersPartialShift.size(); i++) {
+            fullShift.getAssignments().add(shiftAssignmentRepository.save(ShiftAssignment.builder()
+                .shiftId(fullShift.getId())
+                .user(usersPartialShift.get(i))
+                .shiftUserRole(i == 0 ? EShiftUserRole.OVERSEER
+                                      : EShiftUserRole.PARTICIPANT)
+                .build()));
         }
+        shiftRepository.saveAndFlush(partialShift);
+
 
         log.info("Complete DB initialization 💿");
     }
