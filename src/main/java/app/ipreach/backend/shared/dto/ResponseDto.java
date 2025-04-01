65f840c177d65518660aaca3b377329d25915841
L <eleaz.rs@gmail.com>
2025-04-01 08:31:52 +0200

    handle Validation Exceptions

--- src/main/java/app/ipreach/backend/shared/dto/ResponseDto.java
+++ src/main/java/app/ipreach/backend/shared/dto/ResponseDto.java
@@ -2,5 +2,5 @@ package app.ipreach.backend.shared.dto;
 
 import lombok.Builder;
 
-@Builder
+@Builder(toBuilder = true)
 public record ResponseDto(String status, String httpStatus, int code, Object data, String message) { }
