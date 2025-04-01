166e8c81a2fb95a00e89fe23e3ba850494f6aa25
L <eleaz.rs@gmail.com>
2025-04-01 08:31:34 +0200

    ApiError and ApiSubError now are records

--- src/main/java/app/ipreach/backend/core/exception/custom/ApiError.java
+++ src/main/java/app/ipreach/backend/core/exception/custom/ApiError.java
@@ -2,9 +2,7 @@ package app.ipreach.backend.core.exception.custom;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Singular;
 import org.springframework.http.HttpStatusCode;
 
@@ -12,38 +10,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
 @Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ApiError {
-
-    private UUID id;
-
-    private LocalDateTime timestamp;
-
-    private Integer status;
-
-    private HttpStatusCode httpStatus;
-
-    private String endpointPath;
-
-    @JsonProperty("exceptionClassThrown")
-    private String exceptionClazzThrown;
-
-    @JsonProperty("class")
-    private String clazz;
-
-    private String method;
-
-    private Integer line;
-
-    private String message;
-
-    private ApiError rootCause;
-
-    @Singular("error")
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private List<ApiSubError> errors;
-
-}
+public record ApiError (
+    UUID id,
+    LocalDateTime timestamp,
+    Integer status,
+    HttpStatusCode httpStatus,
+    String endpointPath,
+    @JsonProperty("exceptionClassThrown") String exceptionClazzThrown,
+    @JsonProperty("class") String clazz,
+    String method,
+    Integer line,
+    String message,
+    ApiError rootCause,
+    @Singular("error") @JsonInclude(JsonInclude.Include.NON_EMPTY) List<ApiSubError> errors)
+{ }
--- src/main/java/app/ipreach/backend/core/exception/custom/ApiSubError.java
+++ src/main/java/app/ipreach/backend/core/exception/custom/ApiSubError.java
@@ -1,22 +1,8 @@
 package app.ipreach.backend.core.exception.custom;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
 @Builder
-public class ApiSubError {
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String object;
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String field;
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private Object rejectedValue;
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String message;
-
-}
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record ApiSubError(String object, String field, Object rejectedValue, String message) { }
